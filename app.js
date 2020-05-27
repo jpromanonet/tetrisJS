7b80511cdf03f60d3e66e6178634e5d3dfad5a09
jpromanonet <programming@jpromano.net>
2020-05-27 13:38:14 -0600

    JS grid

--- app.js
+++ app.js
@@ -1,4 +1,7 @@
 document.addEventListener('DOMcContentLoaded',() => {
     // Here goes all your code
-
+    // We define the grid size for the game in JS
+    const GRID_WIDTH = 20
+    const GRID_HEIGHT = 40
+    const GRID_SIZE = GRID_WIDTH * GRID_HEIGHT
 })
