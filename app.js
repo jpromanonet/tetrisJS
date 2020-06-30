1e2e0e3ad976551b45e5eacbf578a9866c1a2d7a
jpromanonet <programming@jpromano.net>
2020-06-30 11:39:10 -0600

    Tetronimos

--- app.js
+++ app.js
@@ -5,4 +5,11 @@ document.addEventListener('DOMcContentLoaded',() => {
     const grid = document.querySelector
     const width = 20
     const height = 40
+
+    // The Tetrominos
+    const l Tetromino = [
+        [1, width+1, width*2+1, 2],
+        [width, width+1, width+2, width*2+2],
+    ]
+    
 })
