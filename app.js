3b66dc92ae16e828797d3c23ab59d9a13bb4d277
jpromanonet <programming@jpromano.net>
2020-07-02 11:40:00 -0600

    Tetronimo O start

--- app.js
+++ app.js
@@ -26,5 +26,9 @@ document.addEventListener('DOMcContentLoaded',() => {
         [1, width+1, width+2, width*2+1],
         [width, width+1, width+2, width*2+1],
         [1, width, width+1, width*2+1]
-    ],
+    ]
+
+    const oTetromino = [
+        [0,]
+    ]
 })
