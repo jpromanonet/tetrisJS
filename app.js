6470255b7bc205aac223811b1c2529878705189c
jpromanonet <programming@jpromano.net>
2020-07-02 11:39:05 -0600

    Tetronimo T

--- app.js
+++ app.js
@@ -21,5 +21,10 @@ document.addEventListener('DOMcContentLoaded',() => {
         [width+1, width+2, width*2, width*2+1]
     ]
 
-    const tTetromino = []
+    const tTetromino = [
+        [1, width, width+1, width*2+1],
+        [1, width+1, width+2, width*2+1],
+        [width, width+1, width+2, width*2+1],
+        [1, width, width+1, width*2+1]
+    ],
 })
