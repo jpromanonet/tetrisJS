16600fccad07a5d56c88865e59e44be81ba031b0
jpromanonet <programming@jpromano.net>
2020-07-05 14:27:30 -0300

    Tetromino O

--- app.js
+++ app.js
@@ -1,34 +1,37 @@
-document.addEventListener('DOMcContentLoaded',() => {
-    // Here goes all your code
-    // We define the grid size for the game in JS
+document.addEventListener("DOMcContentLoaded", () => {
+  // Here goes all your code
+  // We define the grid size for the game in JS
 
-    const grid = document.querySelector
-    const width = 20
-    const height = 40
+  const grid = document.querySelector;
+  const width = 20;
+  const height = 40;
 
-    // The Tetrominos
-    const lTetromino = [
-        [1, width+1, width*2+1, 2],
-        [width, width+1, width+2, width*2+2],
-        [1, width+1, width*2+1, width*2],
-        [width, width*2, width*2+1, width2*2]
-    ]
+  // The Tetrominos
+  const lTetromino = [
+    [1, width + 1, width * 2 + 1, 2],
+    [width, width + 1, width + 2, width * 2 + 2],
+    [1, width + 1, width * 2 + 1, width * 2],
+    [width, width * 2, width * 2 + 1, width2 * 2],
+  ];
 
-    const zTetromino = [
-        [0, width, width+1, width*2+1],
-        [width+1, width+2, width*2, width*2+1],
-        [0, width, width+1, width*2+1],
-        [width+1, width+2, width*2, width*2+1]
-    ]
+  const zTetromino = [
+    [0, width, width + 1, width * 2 + 1],
+    [width + 1, width + 2, width * 2, width * 2 + 1],
+    [0, width, width + 1, width * 2 + 1],
+    [width + 1, width + 2, width * 2, width * 2 + 1],
+  ];
 
-    const tTetromino = [
-        [1, width, width+1, width*2+1],
-        [1, width+1, width+2, width*2+1],
-        [width, width+1, width+2, width*2+1],
-        [1, width, width+1, width*2+1]
-    ]
+  const tTetromino = [
+    [1, width, width + 1, width * 2 + 1],
+    [1, width + 1, width + 2, width * 2 + 1],
+    [width, width + 1, width + 2, width * 2 + 1],
+    [1, width, width + 1, width * 2 + 1],
+  ];
 
-    const oTetromino = [
-        [0, 1, width, width+1],
-    ]
-})
+  const oTetromino = [
+    [0, 1, width, width + 1],
+    [0, 1, width, width + 1],
+    [0, 1, width, width + 1],
+    [0, 1, width, width + 1],
+  ];
+});
